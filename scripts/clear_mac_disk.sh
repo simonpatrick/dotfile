e9f6b9ff419f816cb9a5804d0ad89d6dbc3f1987
patrick <patrick@patrickdeMacBook-Pro.local>
2016-05-22 23:24:23 +0800

    update clear mac disk scripts

--- scripts/clear_mac_disk.sh
+++ scripts/clear_mac_disk.sh
@@ -3,16 +3,16 @@
 # remove RUBY cache
 echo "remove ruby 2.2.0 cache"
 RUBY_VERSION=2.2.0
-rm -rf /usr/local/lib/ruby/gems/$RUBY_VERSION/cache
+rm -rf /usr/local/lib/ruby/gems/$RUBY_VERSION/cache/*
 
 # clearn /private/folder
 
-sudo rm -rf /private/var/folders
-sudo rm -rf /private/var/log
+sudo rm -rf /private/var/folders/*
+sudo rm -rf /private/var/log/*
 
 ## clean /Library
 echo "clear cache in "
-sudo rm -rf /Library/Caches
+sudo rm -rf /Library/Caches/*
 
 # clearn logs
 echo "clear Library logs"
@@ -22,13 +22,13 @@ sudo rm -rf /Library/Logs/*
 
 echo "remove current users caches"
 echo "HOME DIR is $HOME"
-rm -rf $HOME/Library/Caches
-rm -rf $HOME/Library/Logs
+sudo rm -rf $HOME/Library/Caches/*
+sudo rm -rf $HOME/Library/Logs/*
 
 # clean gradle caches
 echo "remove gradle caches"
-rm -rf ~/.gradle/caches/*
+sudo rm -rf ~/.gradle/caches/*
 
 # clear caches
 echo "remove caches "
-rm -rf ~/.cache/*
+sudo rm -rf ~/.cache/*
